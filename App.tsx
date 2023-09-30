701870f8c213cc6a85ab92bd6ea1efe55d1363ce
Kszesiek <kszesiek@gmail.com>
2023-09-30 19:21:01 +0200

    Basic tab navigator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {MaterialCommunityIcons, Ionicons} from '@expo/vector-icons';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function CalendarScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="auto"/>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={() => ({
+            // tabBarActiveTintColor: 'tomato',
+            // tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            options={() => ({
+              tabBarIcon: ({focused, color, size}) => {
+                const iconName = focused ? "home" : "home-outline";
+
+                return <Ionicons name={iconName} size={size} color={color}/>;
+                // format-list-text
+              }
+            })}
+            component={HomeScreen}
+          />
+          <Tab.Screen
+            name="Wydarzenia"
+            options={() => ({
+              tabBarIcon: ({focused, color, size}) => {
+                const iconName = focused ? "format-list-text" : "format-list-text";
+
+                return <MaterialCommunityIcons name={iconName} size={size} color={color}/>;
+              }
+            })}
+            component={CalendarScreen}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
